205fb38d5271bf58205cd38f32d8c904c34e83ff
rajeshd7532-a11y <rajeshd7532@gmail.com>
2026-03-30 19:58:30 +0530

    Add files via upload

---
+++ day3.py.py
@@ -0,0 +1,42 @@
+'''list_1 = [1,2,3,4,"rajesh",["lavanya rajesh"],"friends",133,33]
+print(list_1[5][0:13])
+
+print(9+8)
+print("python" + "language")
+print([1,2] + [3,4])
+
+ # CONCATENATION
+ ------------------- 
+ THIS is nothing but' a(+) be have
+ case:- 1
+ ---------
+ integers----- this will act as addition for the int
+
+ case:2
+ -------
+ For other data types [we have use same type]this (+)
+ act as concatenation
+ 
+print("ram" +  "rathod")
+
+ #TUPLE---> IS a collection of different datatypes and this is represents
+ by (), separated by (,)
+
+  example---->
+  -------------
+
+t = (12,89,"python",(23,"rajesh", [67,"python is a lanbguage",(7,8)],["python",[34,9]]))
+print(t[5])
+
+num = 9
+num_2 = 90
+print(f"before swapping in num={num} and num_2={num_2}")
+num,num_2 = num_2,num
+print(f"after swapping in num={num} and num_2={num_2}")
+'''
+leap_year = int(input("enter year: "))
+if (leap_year % 4== 0 and leap_year %100!=0) or leap_year % 400==0:
+    print('leap year')
+else: print('not a leap')
+
+    
