b6209aef59f1fde6993e24d299a44671a719b7a4
rajeshd7532-a11y <rajeshd7532@gmail.com>
2026-04-08 13:08:26 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,53 @@
+'''
+BREAK--->
+-----------
+so = "codegnan"
+for i in range(1,10):
+    print(i)
+    print(so)
+    if i == 8 :
+        break
+
+list = [1,2,3,4,5]
+for n in list:
+    print(n)
+    if n == 5:
+        break
+
+for i in range(1,10):
+    if i == 5:
+        continue
+    print(i)
+    
+# CONTINUE
+------------
+list = [1,2,3,5,"rajesh","pinky"]
+for i in list:
+    if i == "pinky":
+     continue
+    print(i)
+
+ad = "rajesh rocky rock"
+for j in ad:
+    if j == "r":
+        continue
+    print(j)
+'''
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
