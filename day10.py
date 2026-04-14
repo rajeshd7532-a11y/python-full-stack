710c7e5f593c044323ddff8e34a313a85c4cb030
rajeshd7532-a11y <rajeshd7532@gmail.com>
2026-04-14 12:48:49 +0530

    built-in and inbuilt

---
+++ day10.py
@@ -0,0 +1,32 @@
+'''                          Types of Modules
+                            -----------------
+        1. Built-in or Inbuilt      2.User define modules
+
+import day9 
+print(day9.sbi_atchuth_)
+
+#user-define module
+---------------------
+THis is developed by the user or programmer imside a file of python coding
+and used by called import with file name...
+
+#syntax--->
+----------- import (keyword)file_name
+            file_name.functionality
+
+#built-in or inbuilt
+---------------------
+----->Already these are comes with installation and they are ready to use in the
+program
+---->This is developed by the developer
+'''
+import math
+print(math.sqrt(81))
+import random
+print(random.randint(1,100))
+
+
+
+
+
+   
