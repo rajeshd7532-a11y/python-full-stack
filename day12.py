9d6472791fdce482808e2a01bd08040d6d41869e
rajeshd7532-a11y <rajeshd7532@gmail.com>
2026-04-16 12:49:16 +0530

    oop's

---
+++ day12.py
@@ -0,0 +1,55 @@
+'''OOP'S
+------->
+---->#object-orient language(oop)is a style of programming where we model real-world
+things as objects as objects that contain both data and function()
+
+-----> resuable of code
+-----> And also scalable
+
+class
+-----
+--->   class is a blue-print or template that defines what kind of the data and
+   behavior a certain type of objects will have
+object
+------
+----> This  is instance of class or an object is a real instance created from a
+   class. it is the actual thing exists in memory while the program run
+
+
+class Car:
+    def __init__(self,brand,color):
+        self.brand= brand
+        self.color= color
+car_1 = Car("TATA","black")
+car_2 = Car("THAR","red")
+print(car_1.brand,car_1.color)
+
+print(car_2.brand,car_2.color)
+
+class bike:
+    def __init__(self,brand,color,name):
+        self.brand = brand
+        self.color = color
+        self.name = name
+bike_1 = bike("RX100","red","rahul")
+bike_2 = bike("yamaha","blue","ramu")
+print(bike_1.brand,bike_1.color,'and',bike_1.name)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    
